50710e03fb18ba5f36e4966bf99427d74bed24f9
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-04-15 19:38:41 +0700

    Create 169. Majority Element.py

---
+++ LeetCode/Arrays & Hashing/169. Majority Element.py
@@ -0,0 +1,22 @@
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+# Example 1:
+# Input: nums = [3,2,3]
+# Output: 3
+
+# Example 2:
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+      
+        hm = defaultdict(int)
+        for i in range(len(nums)) :
+           hm[nums[i]] += 1
+
+        for key , value in hm.items():
+            if value > len(nums)/2 :
+                return key
+        return 0
