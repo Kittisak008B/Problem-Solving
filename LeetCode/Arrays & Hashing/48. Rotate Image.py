b68e70e6f59d6e14a072e49bbcdc1b8886890896
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-05-09 02:29:47 +0700

    Create 48. Rotate Image.py

---
+++ LeetCode/Arrays & Hashing/48. Rotate Image.py
@@ -0,0 +1,27 @@
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+# Example 1:
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+# Example 2:
+# Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+# Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # transpose then reverse
+        for i in range(len(matrix)):
+	        for j in range(i+1 , len(matrix)):
+		        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(len(matrix)) :
+            p1 = 0
+            p2 = len(matrix[i]) - 1
+            while p1 < p2 :
+                matrix[i][p1] , matrix[i][p2] = matrix[i][p2] , matrix[i][p1]
+                p1 += 1
+                p2 -= 1
