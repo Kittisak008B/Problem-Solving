b892ea251674cac39c9a909e3e93ec3b0981987e
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-05-28 01:43:50 +0700

    Create 198. House Robber.py

---
+++ LeetCode/Dynamic Programming/198. House Robber.py
@@ -0,0 +1,34 @@
+# You are a professional robber planning to rob houses along a street. 
+# Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected 
+# and it will automatically contact the police if two adjacent houses were broken into on the same night.
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+# Total amount you can rob = 1 + 3 = 4.
+
+# Example 2:
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        for i in range(1 , len(nums)) :
+            if i == 1 :
+                nums[i] = max(nums[i] , nums[i-1])
+            else :
+                nums[i] = max(nums[i]+nums[i-2] , nums[i-1])
+        return nums[-1]
+      
+'''
+Example 2: Input: nums = [2,7,9,3,1]  Output: 12
+                     i=1 [2,7,9,3,1]
+                     i=2 [2,7,11,3,1]
+                     i=3 [2,7,11,11,1]
+                     i=4 [2,7,11,11,12]
+'''
