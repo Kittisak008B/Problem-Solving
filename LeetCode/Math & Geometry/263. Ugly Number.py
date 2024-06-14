75a261dbfc9a21c19775d158d2fb00b0b9fee961
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-06-14 23:24:38 +0700

    Update 263. Ugly Number.py

--- LeetCode/Math & Geometry/263. Ugly Number.py
+++ LeetCode/Math & Geometry/263. Ugly Number.py
@@ -1,15 +1,19 @@
-# An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
-# Given an integer n, return the nth ugly number.
+# An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.  Given an integer n, return true if n is an ugly number.
 
 # Example 1:
-# Input: n = 10
-# Output: 12
-# Explanation: [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the sequence of the first 10 ugly numbers.
+# Input: n = 6
+# Output: true
+# Explanation: 6 = 2 × 3
   
 # Example 2:
 # Input: n = 1
-# Output: 1
+# Output: true
 # Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+  
+# Example 3:
+# Input: n = 14
+# Output: false
+# Explanation: 14 is not ugly since it includes the prime factor 7.
 
 class Solution:
     def isUgly(self, n: int) -> bool:
