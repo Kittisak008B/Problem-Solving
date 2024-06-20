a6b54a9d5cb949b478b318abfa0dc9b0bed90fd0
J_powell <157298910+Kittisak008B@users.noreply.github.com>
2024-06-20 14:30:19 +0700

    Update 1641. Count Sorted Vowel Strings.py

--- LeetCode/Math & Geometry/1641. Count Sorted Vowel Strings.py
+++ LeetCode/Math & Geometry/1641. Count Sorted Vowel Strings.py
@@ -42,25 +42,25 @@ n = 2  C(5,2) = (5!)/(3!*2!) = 5*4/2 = 10 (ae ai ao au ei eo eu io iu ou)
                                        + 5 (aa ee ii oo uu) 
                                     total = 10 + 5 =  15 
 
-n = 3  choose 1 vowel diff -> 5 (aaa eee iii ooo uuu)
-       choose 2 vowel diff ->   ex. a-a-e -> one converting point to change a to e -> C(2,1)
+n = 3  choose 1 distinct vowel  -> 5 (aaa eee iii ooo uuu)
+       choose 2 distinct vowel  ->   ex. a-a-e -> one converting point to change a to e -> C(2,1)
                                 C(5,2)*C(n-1,1)=C(5,2)*C(2,1) = 10*2 = 20 (ae+a ai+a ao+a au+a ei+e eo+e eu+e io+i iu+i ou+o
                                                                            ae+e ai+i ao+o au+u ei+i eo+o eu+u io+o iu+u ou+u )
-       choose 3 vowel diff ->   ex.a-e-i -> two converting point to change a to e and e to i -> C(2,2)
+       choose 3 distinct vowel  ->   ex.a-e-i -> two converting point to change a to e and e to i -> C(2,2)
                                 C(5,3)*C(n-1,k-1)=C(5,3)*C(2,2) = 10 (aei aeo aio eio aeu aiu eiu aou eou iou)
        total = 5 + 20 + 10 = 35
 
-n = 4  choose 1 vowel diff -> 5 (aaaa,eeee,iiii,oooo,uuuu)
-       choose 2 vowel diff ->   a-a-e-e -> one converting point to change a to e -> C(3,1)
+n = 4  choose 1 distinct vowel  -> 5 (aaaa,eeee,iiii,oooo,uuuu)
+       choose 2 distinct vowel  ->   a-a-e-e -> one converting point to change a to e -> C(3,1)
                                 C(5,2)*C(n-1,2)=C(5,2)*C(3,1) = 30 (ae+aa ai+aa ao+aa au.. ei.. eo.. eu.. io.. iu.. ou..
                                                                     ae+ee ai+ii ao+oo au.. ei.. eo.. eu.. io.. iu.. ou..
                                                                     ae+ae ai+ai ao+ao au.. ei.. eo.. eu.. io.. iu.. ou..)
-       choose 3 vowel diff -> a-a-e-i -> two converting point to change a to e and e to i -> C(3,2)
+       choose 3 distinct vowel  -> a-a-e-i -> two converting point to change a to e and e to i -> C(3,2)
                               C(5,3)*C(n-1,k-1)=C(5,3)*C(3,2) = 30
-       choose 4 vowel diff -> C(5,4)*C(n-1,k-1)=C(5,4)*C(3,3) = 5 (aeio aeiu aeou aiou eiou)
+       choose 4 distinct vowel  -> C(5,4)*C(n-1,k-1)=C(5,4)*C(3,3) = 5 (aeio aeiu aeou aiou eiou)
        total = 5 + 30 + 30 + 5 = 70
 ...
-n = 50 : choose 1 vowel diff + choose 2 vowel diff + choose 3 vowel diff + choose 4 vowel diff + choose 5 vowel diff
+n = 50 :choose 1 distinct vowel  + choose 2 vowel  + choose 3 vowel      + choose 4 vowel      + choose 5 vowel 
                    5         + C(5 , 2)*C(n-1 , 1) + C(5 , 3)*C(n-1 , 2) + C(5 , 4)*C(n-1 , 3) + C(5 , 5)*C(n-1 , 4)
 
 ------------------------
@@ -109,13 +109,17 @@ n
 
 class Solution:
     def countVowelStrings(self, n: int) -> int:
+        dp = {}
         def c(n , vowel) :
             if n == 1 :
                 return 1
+            if (n , vowel) in dp :
+                return dp[(n , vowel)]
             count = 0 
             for i in range(vowel , 5) :
                 count += c(n-1 , i)
-            return count
+            dp[(n , vowel)] = count
+            return dp[(n , vowel)]
         return c(n+1 , 0)
 '''
 a e i o u
