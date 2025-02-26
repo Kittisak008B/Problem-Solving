98968757547b9e44e0d0b827bf33ff456346c40f
PHAM <157298910+Kittisak008B@users.noreply.github.com>
2025-02-26 16:46:00 +0700

    Create 1749. Maximum Absolute Sum of Any Subarray.go

---
+++ LeetCode/Arrays & Hashing/1749. Maximum Absolute Sum of Any Subarray.go
@@ -0,0 +1,9 @@
+func maxAbsoluteSum(nums []int) int {
+    res, mn, mx := 0, 0, 0
+	for _, num := range nums {
+		mx = int(math.Max(float64(mx+num), 0))
+		mn = int(math.Min(float64(mn+num), 0))
+		res = int(math.Max(float64(res), math.Max(float64(-mn), float64(mx))))
+	}
+	return res
+}
