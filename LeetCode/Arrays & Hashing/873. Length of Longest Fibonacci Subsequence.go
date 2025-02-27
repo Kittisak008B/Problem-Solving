698db77019318c62921576a26abda696c1fe6d6d
PHAM <157298910+Kittisak008B@users.noreply.github.com>
2025-02-27 21:21:05 +0700

    Create 873. Length of Longest Fibonacci Subsequence.go

---
+++ LeetCode/Arrays & Hashing/873. Length of Longest Fibonacci Subsequence.go
@@ -0,0 +1,23 @@
+func lenLongestFibSubseq(arr []int) int {
+  longest := 0
+	s := make(map[int]bool) 
+	for _, num := range arr {
+		s[num] = true
+	}
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			a, b := arr[i], arr[j]
+			count := 2 
+			for s[a+b] {
+				count++
+				a, b = b, a+b
+			}
+			if count >= 3 { 
+				if count > longest {
+					longest = count
+				}
+			}
+		}
+	}
+	return longest
+}
